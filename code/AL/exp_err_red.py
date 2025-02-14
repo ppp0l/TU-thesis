359b5c0d40fafe2dfe20079f3e95702c781332ca
Paolo Villani <paolo.villani00@outlook.com>
2025-02-14 14:29:29 +0100

    set up lips tol prob

--- code/AL/exp_err_red.py
+++ code/AL/exp_err_red.py
@@ -42,55 +42,6 @@ def exp_upper(alpha, eps, LB, UB) :
     res = np.sum(comp, axis = 1)
 
     return res
-    
-def exp_lower(beta, eps, LB, UB) :
-
-    comp =np.zeros_like(beta )
-    
-    cond1 = np.array(beta - eps <= LB)
-    cond2 = np.array(beta + eps <= UB )
-
-    c2 = 1/6*(beta-eps-UB)**3
-    comp[ cond1 * cond2 ] = c2  [ cond1 * cond2 ] 
-
-    c3 = 1/6*((beta - UB - eps)**3 - (beta -LB - eps)**3)
-    comp[ cond1 *~cond2 ] = c3 [ cond1 *~cond2 ]
-
-    c1 = 1/6*((beta - UB - eps)**3 - (beta - UB + eps)**3)
-    comp[~cond1 * cond2 ] = c1 [~cond1 * cond2 ]
-
-    c4 = 2*eps*(beta *(UB - beta - eps ) + 1/2 *((beta+eps)**2 - UB**2)) - 1/6 * ( (beta -LB - eps)**3 + 8 * eps**3 )
-    comp[~cond1 *~cond2 ] = c4 [~cond1 *~cond2 ]
-
-    c5 = 2*eps*(beta *(UB -LB) + 1/2 *(LB**2 - UB **2))
-
-    comp[beta + eps <= LB] = c5[beta + eps <= LB]
-    comp[beta - eps >= UB] = 0
-
-
-    comp = comp/(2*np.reshape(eps, (1, -1)) * (UB-LB))
-    
-    res = np.sum(comp, axis = 1)
-
-    return res
-
-def test_EER(eps, p, q, lips):
-    _, LB, UB = lips.predict(p, return_bds=True)
-    _, LBq, UBq = lips.predict(q, return_bds=True)
-
-    dist = np.linalg.norm(p-q, ord = 2, axis=1, keepdims=True)
-    L = lips.L
-
-    alpha = UBq - L * dist - eps
-
-    EUI = exp_upper(alpha, eps, LB,  UB)
-
-    beta = - LBq - L * dist - eps
-
-    ELI = - exp_upper(beta,eps,-UB, -LB)
-
-    #print(EUI.mean(), ELI.mean())
-    return EUI - ELI
 
 def grad_exp_upper(alpha, eps, LB, UB) :
 
@@ -175,85 +126,136 @@ def grad_exp_upper(alpha, eps, LB, UB) :
 
     return dalpha, deps, dLB, dUB
 
-def grad_exp_lower(beta, eps, LB, UB) :
 
-    # prova = np.zeros_like(beta)
+def test_EER(eps, p, q, lips):
+    _, LB, UB = lips.predict(p, return_bds=True)
+    _, LBq, UBq = lips.predict(q, return_bds=True)
 
-    n_samples = len(beta)
-    n_out = len(beta[0])
+    dist = np.linalg.norm(p-q, ord = 2, axis=1, keepdims=True)
+    L = lips.L
 
-    dbeta = np.zeros( (n_samples, n_out) )
-    deps = np.zeros( (n_samples, n_out))
-    dLB = np.zeros( (n_samples, n_out) )
-    dUB = np.zeros( (n_samples, n_out))
+    alpha = UBq - L * dist - eps
 
-    cond1 = np.array(beta - eps <= LB)
-    cond2 = np.array(beta + eps <= UB )
+    EUI = exp_upper(alpha, eps, LB,  UB)
 
-    # c2 = 1/6*(beta-eps-UB)**3
-    der = 1/2*(beta-eps-UB)**2  
-    dbeta[ cond1 * cond2 ] = der [ cond1 * cond2 ] 
-    der = 1/2*(beta-eps-UB)**2
-    deps[ cond1 * cond2 ] = der [ cond1 * cond2 ] 
-    dLB[ cond1 * cond2 ] = 0
-    der = - 1/2*(beta-eps-UB)**2
-    dUB[ cond1 * cond2 ] = der [ cond1 * cond2 ] 
-    # prova[ cond1 * cond2 ] = 2
+    beta = - LBq - L * dist - eps
 
-    # c3 = 1/6*((beta - UB - eps)**3 - (beta -LB - eps)**3)
-    der = 1/2*((beta - UB - eps)**2 - (beta - LB - eps)**2)
-    dbeta[ cond1 *~cond2 ] =  der [ cond1 *~cond2 ] 
-    der = 1/2*((beta - UB - eps)**2 - (beta - LB - eps)**2) 
-    deps[ cond1 *~cond2 ] = der [ cond1 *~cond2 ] 
-    der = 1/2*(beta - eps - LB)**2
-    dLB[ cond1 *~cond2 ] = der  [ cond1 *~cond2 ] 
-    der = - 1/2*(beta - eps - UB)**2
-    dUB[ cond1 *~cond2 ] = der  [ cond1 *~cond2 ] 
-    # prova[ cond1 *~cond2 ] = 3
+    ELI = - exp_upper(beta,eps,-UB, -LB)
+
+    #print(EUI.mean(), ELI.mean())
+    return EUI - ELI
+
+
+# def exp_lower(beta, eps, LB, UB) :
+
+#     comp =np.zeros_like(beta )
     
-    # c1 = 1/6*((beta - UB - eps)**3 - (beta - UB + eps)**3)
-    der = 1/2*((beta - UB - eps)**2 - (beta - UB + eps)**2)
-    dbeta[~cond1 * cond2 ] =  der [~cond1 * cond2 ] 
-    der = 1/2*((beta - UB - eps)**2 + (beta - UB + eps)**2) 
-    deps[~cond1 * cond2 ] = der [~cond1 * cond2 ] 
-    dLB[~cond1 * cond2 ] = 0
-    der = - 1/2*((beta - UB - eps)**2 - (beta - UB + eps)**2)
-    dUB[~cond1 * cond2 ] = der  [~cond1 * cond2 ] 
-    # prova[~cond1 * cond2 ] = 1
+#     cond1 = np.array(beta - eps <= LB)
+#     cond2 = np.array(beta + eps <= UB )
 
-    # c4 = 2*eps*(beta *(UB - beta - eps ) + 1/2 *((beta+eps)**2 - UB**2)) - 1/6 * ( (beta -LB - eps)**3 + 8 * eps**3 )
-    der = 2*eps*( UB - beta ) - 1/2 * (beta -LB - eps)**2
-    dbeta[~cond1 *~cond2 ] =  der  [~cond1 *~cond2 ] 
-    der = 2*(beta *(UB - beta - eps ) + 1/2 *((beta+eps)**2 - UB**2)) + 2*eps**2 - 1/2 *( - (beta -LB - eps)**2  + 8*eps**2)
-    deps[~cond1 *~cond2 ] = der  [~cond1 *~cond2 ] 
-    der = 1/2 * (beta - LB - eps)**2
-    dLB[~cond1 *~cond2 ] = der  [~cond1 *~cond2 ] 
-    der = 2*eps*( beta - LB )
-    dUB[~cond1 *~cond2 ] = der [~cond1 *~cond2 ] 
-    # prova[~cond1 *~cond2 ] = 4
+#     c2 = 1/6*(beta-eps-UB)**3
+#     comp[ cond1 * cond2 ] = c2  [ cond1 * cond2 ] 
 
-    # print(prova)
+#     c3 = 1/6*((beta - UB - eps)**3 - (beta -LB - eps)**3)
+#     comp[ cond1 *~cond2 ] = c3 [ cond1 *~cond2 ]
 
-    cond3 = np.array(beta + eps <= LB)
-    if np.any(cond3) :
-    # c5 = 2*eps*(beta *(UB -LB) + 1/2 *(LB**2 - UB**2))
-        der = np.ones((len(cond3), 1) ) *2*eps *(UB -LB)
-        dbeta[cond3] =  der  [cond3]
-        der = 2*(beta *(UB -LB) + 1/2 *(LB**2 - UB**2))
-        deps[cond3] = der [cond3] 
-        der = 2*eps*(- beta + LB )
-        dLB[cond3] = der [cond3] 
-        der = 2*eps*( beta - UB )
-        dUB[cond3] = der [cond3] 
-    # prova[cond3] = 5
+#     c1 = 1/6*((beta - UB - eps)**3 - (beta - UB + eps)**3)
+#     comp[~cond1 * cond2 ] = c1 [~cond1 * cond2 ]
 
-    cond4 = np.array(beta - eps >= UB)
-    if np.any(cond4) :
-        dbeta[cond4] = 0
-        deps[cond4] = 0
-        dLB[cond4] = 0
-        dUB[cond4] = 0
-    # prova[cond4] = 6
-    # print(prova)
+#     c4 = 2*eps*(beta *(UB - beta - eps ) + 1/2 *((beta+eps)**2 - UB**2)) - 1/6 * ( (beta -LB - eps)**3 + 8 * eps**3 )
+#     comp[~cond1 *~cond2 ] = c4 [~cond1 *~cond2 ]
 
-    return dbeta, deps, dLB, dUB
+#     c5 = 2*eps*(beta *(UB -LB) + 1/2 *(LB**2 - UB **2))
+
+#     comp[beta + eps <= LB] = c5[beta + eps <= LB]
+#     comp[beta - eps >= UB] = 0
+
+
+#     comp = comp/(2*np.reshape(eps, (1, -1)) * (UB-LB))
+    
+#     res = np.sum(comp, axis = 1)
+
+#     return res
+
+# def grad_exp_lower(beta, eps, LB, UB) :
+
+#     # prova = np.zeros_like(beta)
+
+#     n_samples = len(beta)
+#     n_out = len(beta[0])
+
+#     dbeta = np.zeros( (n_samples, n_out) )
+#     deps = np.zeros( (n_samples, n_out))
+#     dLB = np.zeros( (n_samples, n_out) )
+#     dUB = np.zeros( (n_samples, n_out))
+
+#     cond1 = np.array(beta - eps <= LB)
+#     cond2 = np.array(beta + eps <= UB )
+
+#     # c2 = 1/6*(beta-eps-UB)**3
+#     der = 1/2*(beta-eps-UB)**2  
+#     dbeta[ cond1 * cond2 ] = der [ cond1 * cond2 ] 
+#     der = 1/2*(beta-eps-UB)**2
+#     deps[ cond1 * cond2 ] = der [ cond1 * cond2 ] 
+#     dLB[ cond1 * cond2 ] = 0
+#     der = - 1/2*(beta-eps-UB)**2
+#     dUB[ cond1 * cond2 ] = der [ cond1 * cond2 ] 
+#     # prova[ cond1 * cond2 ] = 2
+
+#     # c3 = 1/6*((beta - UB - eps)**3 - (beta -LB - eps)**3)
+#     der = 1/2*((beta - UB - eps)**2 - (beta - LB - eps)**2)
+#     dbeta[ cond1 *~cond2 ] =  der [ cond1 *~cond2 ] 
+#     der = 1/2*((beta - UB - eps)**2 - (beta - LB - eps)**2) 
+#     deps[ cond1 *~cond2 ] = der [ cond1 *~cond2 ] 
+#     der = 1/2*(beta - eps - LB)**2
+#     dLB[ cond1 *~cond2 ] = der  [ cond1 *~cond2 ] 
+#     der = - 1/2*(beta - eps - UB)**2
+#     dUB[ cond1 *~cond2 ] = der  [ cond1 *~cond2 ] 
+#     # prova[ cond1 *~cond2 ] = 3
+    
+#     # c1 = 1/6*((beta - UB - eps)**3 - (beta - UB + eps)**3)
+#     der = 1/2*((beta - UB - eps)**2 - (beta - UB + eps)**2)
+#     dbeta[~cond1 * cond2 ] =  der [~cond1 * cond2 ] 
+#     der = 1/2*((beta - UB - eps)**2 + (beta - UB + eps)**2) 
+#     deps[~cond1 * cond2 ] = der [~cond1 * cond2 ] 
+#     dLB[~cond1 * cond2 ] = 0
+#     der = - 1/2*((beta - UB - eps)**2 - (beta - UB + eps)**2)
+#     dUB[~cond1 * cond2 ] = der  [~cond1 * cond2 ] 
+#     # prova[~cond1 * cond2 ] = 1
+
+#     # c4 = 2*eps*(beta *(UB - beta - eps ) + 1/2 *((beta+eps)**2 - UB**2)) - 1/6 * ( (beta -LB - eps)**3 + 8 * eps**3 )
+#     der = 2*eps*( UB - beta ) - 1/2 * (beta -LB - eps)**2
+#     dbeta[~cond1 *~cond2 ] =  der  [~cond1 *~cond2 ] 
+#     der = 2*(beta *(UB - beta - eps ) + 1/2 *((beta+eps)**2 - UB**2)) + 2*eps**2 - 1/2 *( - (beta -LB - eps)**2  + 8*eps**2)
+#     deps[~cond1 *~cond2 ] = der  [~cond1 *~cond2 ] 
+#     der = 1/2 * (beta - LB - eps)**2
+#     dLB[~cond1 *~cond2 ] = der  [~cond1 *~cond2 ] 
+#     der = 2*eps*( beta - LB )
+#     dUB[~cond1 *~cond2 ] = der [~cond1 *~cond2 ] 
+#     # prova[~cond1 *~cond2 ] = 4
+
+#     # print(prova)
+
+#     cond3 = np.array(beta + eps <= LB)
+#     if np.any(cond3) :
+#     # c5 = 2*eps*(beta *(UB -LB) + 1/2 *(LB**2 - UB**2))
+#         der = np.ones((len(cond3), 1) ) *2*eps *(UB -LB)
+#         dbeta[cond3] =  der  [cond3]
+#         der = 2*(beta *(UB -LB) + 1/2 *(LB**2 - UB**2))
+#         deps[cond3] = der [cond3] 
+#         der = 2*eps*(- beta + LB )
+#         dLB[cond3] = der [cond3] 
+#         der = 2*eps*( beta - UB )
+#         dUB[cond3] = der [cond3] 
+#     # prova[cond3] = 5
+
+#     cond4 = np.array(beta - eps >= UB)
+#     if np.any(cond4) :
+#         dbeta[cond4] = 0
+#         deps[cond4] = 0
+#         dLB[cond4] = 0
+#         dUB[cond4] = 0
+#     # prova[cond4] = 6
+#     # print(prova)
+
+#     return dbeta, deps, dLB, dUB
--- code/AL/tolerance/lips.py
+++ code/AL/tolerance/lips.py
@@ -1,7 +1,90 @@
 import numpy as np
-from scipy.optimize import minimize
-from AL import exp_err_red
+from scipy.optimize import minimize, LinearConstraint, Bounds
 
-def tol_prob(W, curr_tol, candidates, samples, lips ) :
-    # this is going to be so damn hard lol
+from models.lipschitz import lipschitz_regressor
+from AL import exp_err_red
+from AL.tolerance.GP import get_multistarts
+
+def solve_acc_prob(W, candidates, samples, lips : lipschitz_regressor, cost ) :
+    n_cands = len(candidates)
+
+    training_p = lips.train_x
+    current_accs = np.mean(lips.noise, axis = 1)
+
+    target_pts = np.concatenate( (training_p, candidates), axis = 0)
+    current_accs = np.concatenate( (current_accs, np.zeros(n_cands) ))
+    current_precs = current_accs**(-1/cost) 
+
+    _, LB_p, UB_p = lips.predict(target_pts, return_bds=True)
+
+    samples2 = np.sum(samples**2, axis = 1, keepdims=True)
+    tr_x2 = np.sum(target_pts**2, axis = 1, keepdims=True) 
+    samplesxT = samples.dot(target_pts.transpose())
+    dist_x = np.sqrt( samples2 + tr_x2.transpose() - 2 *samplesxT )
+    dshape = dist_x.shape
+    Ldist = np.outer(dist_x.reshape(-1 ), lips.L).reshape( (dshape[0],dshape[1],lips.dout))
+    
+    # budget constraint, spend all of the budget
+    budget_constraint = LinearConstraint( np.ones_like(), lb = np.sum(current_precs) + W, ub = np.sum(current_precs) + W)
+    # old points and positivity constraint, cannot decrease acccuracy in the old points
+    old_points_constraint = Bounds(lb = current_precs, keep_feasible = True )
+
+    starts = get_multistarts(n_cands, W, current_precs)
+    
+    iter_options = { 'maxiter' : 200,
+                    'ftol' : 1.0e-8
+                    }
+    
+    res_list = []
+    
+    # iterate over starts
+    for j, start in enumerate(starts) :
+        # scipy solve
+        res = minimize( acc_prob_target, start, args = (LB_p, UB_p, samples, Ldist, lips, cost), 
+                                        method='SLSQP',
+                                        jac = acc_prob_grad,
+                                        constraints = (budget_constraint), 
+                                        bounds = old_points_constraint,
+                                        options = iter_options
+                                        )
+        
+        res_list.append(res)
+
+    # dictionaries with optimization data  
+    # acc_sol_pars=res_list
+    
+    # recover best solution
+    best =np.inf
+    best_precs = current_precs
+    for res in res_list :
+        if res.fun < best :
+            best = res['fun']
+            best_precs = res['x']
+            best_res = res
+    print(f'Best work distribution: {best_res}')
+
+    # updated points, with threshold
+    updated = (best_precs - current_precs) > 0.02*W
+    n_tr_pts = len(training_p)
+    print(f'Number of updated points: {np.sum(updated[:n_tr_pts])}' )
+    print(f'Number of included candidates: {np.sum(updated[n_tr_pts:])}' )
+    # non updated points
+    best_precs[~updated] = current_precs[~updated]
+    # spread evenly work from non updated
+    best_precs[updated] += (W - np.sum(best_precs - current_precs))/(len(best_precs[updated]))
+    
+    # get best candidates, discards not included ones
+    best_candidates = candidates[best_precs[n_tr_pts:]>0]
+    # recovers tolerance
+    best_accs = best_precs[best_precs > 0]**(-1/cost)
+    
+    return best_accs, best_candidates, updated[:n_tr_pts]
+
+def acc_prob_target(precs, LB_p, UB_p, samples, Ldist, lips, cost):
+
+    accs = precs[precs>0]**(-1/cost)
+
+
+
+def acc_prob_grad(precs, LB_p, UB_p, samples, Ldist, lips, cost):
     pass
